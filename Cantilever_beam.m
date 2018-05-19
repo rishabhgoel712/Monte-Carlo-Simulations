8ce518a032476fbeb6c97e991d7d231440d5d2f8
rishabhgoel712 <rishabhgoel712@gmail.com>
2018-05-19 19:48:02 +0530

    example 1

--- Cantilever_beam.m
+++ Cantilever_beam.m
@@ -1,10 +1,7 @@
 l=0.1;
 nsamples=10000;
-for i=1:nsamples
-    force=1000+50*rand(1);
-    dia=0.01+0.001*rand(1);
-    modulos=10^9 + rand(1)*10^9;
-    inertia=pi*dia^4/64;
-    displacement=force*l^3/3/modulos/inertia;
-end
-
+force=1000+50*rand(nsamples,1);
+dia=0.01+0.001*rand(nsamples,1);
+modulos=10^9 + rand(nsamples,1)*10^9;
+inertia=pi*dia.^4/64;
+displacement=force.*l^3/3./modulos./inertia;
