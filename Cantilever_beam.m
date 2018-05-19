0617f1a1d8f1e9ca6d58b2417cd3d1c45ab7773e
rishabhgoel712 <rishabhgoel712@gmail.com>
2018-05-19 16:29:45 +0530

    v2

--- Cantilever_beam.m
+++ Cantilever_beam.m
@@ -1,6 +1,10 @@
 l=0.1;
-force=1000+50*rand(1);
-dia=0.01+0.001*rand(1);
-modulos=10^9 + rand(1)*10^9;
-inertia=pi*dia^4/64;
-displacement=force*l^3/3/modulos/inertia;
+nsamples=10000;
+for i=1:nsamples
+    force=1000+50*rand(1);
+    dia=0.01+0.001*rand(1);
+    modulos=10^9 + rand(1)*10^9;
+    inertia=pi*dia^4/64;
+    displacement=force*l^3/3/modulos/inertia;
+end
+
