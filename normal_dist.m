cde80a4afe3063a8d254c3fde5a45ea4bc423b5d
rishabhgoel712 <rishabhgoel712@gmail.com>
2018-05-26 08:52:28 +0530

    example added

---
+++ normal_dist.m
@@ -0,0 +1,18 @@
+nturns=200;
+p1=rand(1,nturns);
+p2=rand(1,nturns);
+x1=norminv(p1,120,20);
+x2=norminv(p2,100,10);
+y=x1-x2;
+I=zeros(1,nturns)   
+for i=1:nturns
+   if y(i)<0
+       I(i)=1;
+   end
+end
+pf=mean(I)
+mean_y=mean(y)
+std_y=std2(y)
+plot(x1,x2,'o')
+hold on;
+plot(y,x1-x2)
